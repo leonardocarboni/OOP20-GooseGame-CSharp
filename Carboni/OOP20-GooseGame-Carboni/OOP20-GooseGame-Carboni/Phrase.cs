385e0695bb264009af88afeb1967dfb6b769c18a
Leonardo Carboni <carboapple@gmail.com>
2021-04-30 18:32:33 +0200

    Add Phrase Catch Minigame implementation

---
+++ Carboni/OOP20-GooseGame-Carboni/OOP20-GooseGame-Carboni/Phrase.cs
@@ -0,0 +1,24 @@
+﻿namespace OOP20_GooseGame_Carboni
+{
+    internal interface IPhrase
+    {
+        /**
+         * Generates a random sentence.
+         * @return a random sentence.
+        */
+        string GeneratePhrase();
+
+        /**
+         * Confronts the chosen sentence to the one written by the user.
+         * @param textRead - the sentence written by the user.
+         * @return the number of errors in the sentence written.
+         */
+        int CheckText(string textRead);
+
+        /**
+         * [Test] sets a sentence as the phrase.
+         * @param sentence - tha phrase.
+         */
+        void SetPhrase(string sentence);
+    }
+}
---
+++ Carboni/OOP20-GooseGame-Carboni/OOP20-GooseGame-Carboni/PhraseCatchController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP20_GooseGame_Carboni
+{
+    class PhraseCatchController : MinigameController
+    {
+        
+        private readonly IPhrase phrase;
+        private readonly int errors;
+
+        public PhraseCatchController()
+        {
+            PhraseCatchView view = new PhraseCatchView();
+
+            phrase = new PhraseImpl();
+            
+            String sentence = phrase.GeneratePhrase();
+            view.SetPhrase(sentence);
+
+            var textRead = view.GetInputPhrase();
+            errors = phrase.CheckText(textRead);;
+
+        }
+
+        public int getResult()
+        {
+            /*
+             * the game the result is based on the remaining time
+             * and the errors made 
+             * due to the fact that the timer could not be implemented
+             * it returns -errors made
+             */
+            return -errors;
+        }
+    }
+}
