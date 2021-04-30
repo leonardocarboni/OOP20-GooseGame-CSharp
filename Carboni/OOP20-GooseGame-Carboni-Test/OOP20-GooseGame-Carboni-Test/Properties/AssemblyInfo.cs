67aaa4aab488f2b147a8dbe1109130b5dfc21bde
Leonardo Carboni <carboapple@gmail.com>
2021-04-30 18:31:43 +0200

    Add c# test project

---
+++ Carboni/OOP20-GooseGame-Carboni-Test/OOP20-GooseGame-Carboni-Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OOP20-GooseGame-Carboni-Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OOP20-GooseGame-Carboni-Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f0335e3f-2ab9-40a7-9936-0bd4d5c7c467")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Carboni/OOP20-GooseGame-Carboni-Test/OOP20-GooseGame-Carboni-Test/UnitTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OOP20_GooseGame_Carboni_Test
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void PhraseTest()
+        {
+            Phrase p = new PhraseImpl();
+            p.setPhrase("My sentence is correct - 12345");
+
+            Assert.AreEqual(0, p.checkText("My sentence is correct - 12345"));
+            //missing "5" (replaced with "6")
+            Assert.AreEqual(1, p.checkText("My sentence is correct - 12346"));
+            //missing "345"
+            Assert.AreEqual(3, p.checkText("My sentence is correct - 12"));
+            //missing "correct - 12345" [15 chars] replaced with "wrong - 12345" [13 chars]
+            /*  notice how the space after "-" in the wrong sentence is in the same position as
+                the space before "-" in the correct sentence (not counted as an error)  */
+            Assert.AreEqual(14, p.checkText("My sentence is wrong - 12345"));
+            //missing "correct - 12345" [15 chars] replaced with "wrong_-_12345" [13 chars]
+            Assert.AreEqual(15, p.checkText("My sentence is wrong_-_12345"));
+            //missing "correct" replaced with "CORRECT" [only 7 chars missing, the others are the same]
+            Assert.AreEqual(7, p.checkText("My sentence is CORRECT - 12345"));
+            //more charters than expected (counted as errors)
+            Assert.AreEqual(3, p.checkText("My sentence is correct - 12345678"));
+        }
+
+        [TestMethod]
+        public void CableColorTest()
+        {
+            //shows that it only works with the Equals check because CableColor.BLUE is not an
+            //enum but it's a static istance of the object CableColor
+            Assert.AreNotEqual(CableColor.BLUE, CableColor.BLUE);
+            Assert.IsTrue(CableColor.BLUE.Equals(CableColor.BLUE));
+        }
+    }
+}
