02d6f58d3f1a8ec134f4fa7603cc0edd4b4c2337
Andrea Ongaro <andrea.ongaro2@studio.unibo.it>
2021-05-03 19:18:36 +0200

    Create Dice class

---
+++ Ongaro/Ongaro/src/Dice/Dice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ongaro.dice
+{
+    class Dice : IDice
+    {
+        private static readonly int MIN_VALUE_DICE = 1;
+        private static readonly int MAX_VALUE_DICE = 6;
+
+        public int roll()
+        {
+            return new Random().Next(MIN_VALUE_DICE, MAX_VALUE_DICE);
+        }
+    }
+}
---
+++ Ongaro/Ongaro/src/Dice/IDice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ongaro.dice
+{
+    interface IDice
+    {
+        int roll();
+    }
+}
