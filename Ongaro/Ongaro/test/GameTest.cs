d6daff8c0e02bb21f9305545b1081cc9b2b5a8a2
Andrea Ongaro <andrea.ongaro2@studio.unibo.it>
2021-05-03 19:19:50 +0200

    Create game test

---
+++ Ongaro/Ongaro/test/GameTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ongaro.game;
+using Ongaro.player;
+using System.Collections.Generic;
+
+namespace Ongaro.test
+{
+    [TestClass]
+    public class GameTest
+    {
+        [TestMethod]
+        public void TestGame()
+        {
+            IGame g = new Game();
+            List<IPlayer> list = new List<IPlayer>();
+            list.Add(new Player("Ciao", PlayerColor.PINK));
+            list.Add(new Player("Ciao2", PlayerColor.PINK));
+            list.Add(new Player("Ciao4", PlayerColor.PINK));
+
+            g.Start(list);
+            Assert.IsFalse(g.EndGame());
+            do
+            {
+                g.RollCurrentPlayer();
+            } while (!g.EndGame());
+
+            Assert.IsTrue(g.EndGame());
+        }
+    }
+}
